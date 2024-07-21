d826926f93f6aa56cbdca8df48d90303ba53a255
Godswill David <davidgodswill@gmail.com>
2024-07-21 14:55:45 +0100

    updated codebase - added events and delegate for notification

--- LibraryManagement.BusinessLogic/Service/LibraryService.cs
+++ LibraryManagement.BusinessLogic/Service/LibraryService.cs
@@ -3,33 +3,75 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibraryManagement.Data.Data;
 using LibraryManagement.Data.Entity;
 using LibraryManagement.Data.Service;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace LibraryManagement.BusinessLogic.Service
 {
     public class LibraryService : ILibraryService
     {
         private List<LibraryItem> _libraryItems = new List<LibraryItem>();
+        private readonly LibraryContext _libraryContext;
+
+        public LibraryService(LibraryContext libraryContext)
+        {
+            _libraryContext = libraryContext;
+        }
+
+        public delegate void Notify(string message);
+
+        public event Notify? OnBookAdded;
+
+        public event Notify? OnBookRemoved;
+
+        public event Notify? OnBorrowLibraryItem;
+
+        public event Notify? OnReturnLibraryItem;
+
+        // delegate use
+        private Notify notification = new Notify(OnNewBookAdded);
+
+        public static void OnNewBookAdded(string title)
+        {
+            // some things happens here ...
+
+            // raise the book added event here ...
+            Console.WriteLine($"A book with title: {title} was added to our library!");
+        }
 
         public async Task<Book> AddBookToLibrary(Book book)
         {
-            // remove stub code ...
+            _libraryContext.Books.Add(book);
 
-            LibraryItem libraryItem = new LibraryItem();
-            libraryItem.Books.Add(book);
+            string msg = $"A book with title: {book.Title} was added to our library!";
+            OnBookAdded?.Invoke(msg);
 
             return book;
         }
 
         public async Task<Book> UpdateBook(Book book)
         {
-            throw new NotImplementedException();
+            // LINQ: Language integrated query
+            // C#: var existingBook = _libraryContext.Books.Where(b => b.Id.Equals(book.Id)).SingleOrDefault();
+            // SQL: SELECT * FROM BOOK WHERE Id = book.Id;
+
+            var existingBook = _libraryContext.Books.Where(b => b.Id.Equals(book.Id)).SingleOrDefault();
+
+            var b = _libraryContext.Books.Update(book);
+            return book;
         }
 
         public async Task<Book> DeleteBook(long Id)
         {
-            throw new NotImplementedException();
+            var existingBook = _libraryContext.Books.Where(b => b.Id.Equals(Id)).SingleOrDefault();
+            string msg = $"A book with title: {existingBook.Title} is deleted from to our library!";
+            OnBookRemoved?.Invoke(msg);
+
+            _libraryContext.Books.Remove(existingBook);
+
+            return existingBook;
         }
 
         public async Task<Book> CreateBook(Book book)
--- LibraryManagement.Presentation/Program.cs
+++ LibraryManagement.Presentation/Program.cs
@@ -1,5 +1,6 @@
 ﻿using LibraryManagement.BusinessLogic.Service;
 using LibraryManagement.Data.Entity;
+using LibraryManagement.Data.Service;
 
 Console.WriteLine("Welcome to C# group Library Management System!");
 Console.WriteLine("How can I be of service?");
@@ -9,3 +10,6 @@ Book book = new Book()
     Id = 1,
     Title = "Things fall apart"
 };
+
+ILibraryService libraryService = new LibraryService(new());
+libraryService.AddBookToLibrary(book);
