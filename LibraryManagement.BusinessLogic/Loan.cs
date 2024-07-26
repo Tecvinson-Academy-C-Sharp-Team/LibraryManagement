fd0d6cb6ee2589ccbc7cab68c6f3ef586d7841bc
Tomsilva <tandor222@gmail.com>
2024-07-26 07:22:08 -0700

    Created the core setup for member and Loan classes

---
+++ LibraryManagement.BusinessLogic/Loan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.BusinessLogic
+{
+    public class Loan
+    {
+        public long MemberID { get; set; }
+        public long LoanID { get; set; }
+        public long BookID { get; set; }
+        public DateTime LoanDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+
+    }
+}
---
+++ LibraryManagement.Data/Service/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Data.Service
+{
+    internal class IRepository
+    {
+        public IRepository() { }
+
+
+    }
+}
